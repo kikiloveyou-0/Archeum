043c69b6e94e8bdd2defe036f6816bb61ab7da3b
kikiloveyou-0 <73246145+kikiloveyou-0@users.noreply.github.com>
2022-05-10 16:42:57 +0200

    .

--- src/editor.rs
+++ src/editor.rs
@@ -196,8 +196,7 @@ impl Editor {
         self.cursor_position = Position { x, y }
     }
     fn draw_welcome_message(&self) {
-        let mut welcome_message = format!("archeum
-     editor -- version {}", VERSION);
+        let mut welcome_message = format!("archeum editor -- version {}", VERSION);
         let width = self.terminal.size().width as usize;
         let len = welcome_message.len();
         let padding = width.saturating_sub(len) / 2;
